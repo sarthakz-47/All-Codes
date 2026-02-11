f892231ab3e5c81256fd43ecf1dd59edf5dd2575
Sarthak Jadhav <sarthakz.4100@gmail.com>
2026-02-11 20:39:18 +0530

    11 Feb 2026

---
+++ DSA/DivideAndConquer/inbuiltsort.java
@@ -0,0 +1,13 @@
+package DSA.DivideAndConquer;
+
+import java.util.*;
+
+public class inbuiltsort {
+    public static void main(String[] args) {
+        int arr[] = { 5, 5, 7, 32, 21 };
+        Arrays.sort(arr);
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
