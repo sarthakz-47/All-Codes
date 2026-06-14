c412868728e4d4fba183ca3bc5620aa63d3dff52
sarthakz-47 <sarthakz.4100@gmail.com>
2026-06-14 13:44:29 +0530

    Arrays Revision

--- DSA_Rev/ArrayBasics.java
+++ DSA_Rev/ArrayBasics.java
@@ -1,231 +1,283 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
 package DSA_Rev;
 
-class ArrayBasics {
+// import java.util.*;
+class Main {
+
+      public static int passByValue(int n) {
+            return n * n;
+      }
+
+      public static void passByRef(int arr[]) {
+            arr[1] = 200;
+      }
+
       public static void main(String[] args) {
-            // // how to create?
-            // int[] arr = { 1, 2, 3, 4, 5 };
-            // int[] nums = new int[3];
+            // basics of 1D arrays
+            // 1.creation:
+            int[] arr = { 1, 3, 5, 4, 5 };
+            int[] arr1 = new int[5];
 
-            // // how to print?
-            // for (int i = 0; i < arr.length; i++) {
-            // System.out.print(arr[i] + " ");
-            // }
-            // System.out.println();
-            // for (int x : nums) {
-            // System.out.print(x);
-            // }
-            // System.out.println();
+            // 2.length fnx in array:
+            int n = arr.length;
+            System.out.println(n);
 
-            // // how to update?
-            // arr[2] = 10;
-            // for (int i = 0; i < arr.length; i++) {
-            // System.out.print(arr[i] + " ");
-            // }
-            // System.out.println();
-
-            // // how to get largest/smallest element from array?
-            // int[] lar = { 12, 4, 5, 3, 11 };
-            // int largest = lar[0];
-            // for (int i = 1; i < lar.length; i++) {
-            // if (largest < lar[i]) {
-            // largest = lar[i];
-            // }
-            // }
-            // System.out.print(largest);
-            // System.out.println();
-
-            // // how to get sum of elements in array?
-            // int sum = 0;
-            // for (int i = 0; i < lar.length; i++) {
-            // sum += lar[i];
-            // }
-            // System.out.print(sum);
-            // System.out.println();
-
-            // // how to revrese a array?
-            // int start = 0;
-            // int end = lar.length - 1;
-            // while (start <= end) {
-            // int temp = lar[start];
-            // lar[start] = lar[end];
-            // lar[end] = temp;
-            // start++;
-            // end--;
-            // }
-            // for (int num : lar) {
-            // System.out.print(num + " ");
-            // }
-            // System.out.println();
-
-            // // how to search in arrays?
-            // // linear search: goes one by one O(n)
-            // int tar = 5;
-            // for (int i = 0; i < arr.length; i++) {
-            // if (tar == arr[i]) {
-            // System.out.print("element found at index : " + i);
-            // }
-            // }
-            // System.out.println();
-
-            // // binary search: goes by halfing the array O(logn)
-            // int st, en, target;
-            // target = 1;
-            // st = 0;
-            // en = arr.length - 1;
-            // while (st <= en) {
-            // int mid = (st + en) / 2;
-            // if (target == arr[mid]) {
-            // System.out.println("element found at index: " + mid);
-            // break;
-            // } else if (tar < arr[mid]) {
-            // en = mid - 1;
-            // } else {
-            // st = mid + 1;
-            // }
-            // }
-
-            // // // print pairs in array:
-            // int[] ex = { 1, 2, 3 };
-            // for (int i = 0; i < ex.length; i++) {
-            // for (int j = i + 1; j < ex.length; j++) {
-            // System.out.print(ex[i] + "," + ex[j]);
-            // }
-            // System.out.println();
-            // }
-
-            // // print subarrays:
-            // for (int i = 0; i < ex.length; i++) {
-            // for (int j = i; j < ex.length; j++) {
-            // for (int k = i; k <= j; k++) {
-            // System.out.print(ex[k]);
-            // }
-            // System.out.println();
-            // }
-            // System.out.println();
-            // }
-
-            // // max subarray sum:
-            // // 1.brute force approach: O(n^3)
-            // int sm = 0; // {1,2,3}
-            // int maxSum = Integer.MIN_VALUE;
-            // for (int i = 0; i < ex.length; i++) {
-            // for (int j = i; j < ex.length; j++) {
-            // sm = 0;
-            // for (int k = i; k <= j; k++) {
-            // sm += ex[k];
-            // }
-            // if (sm > maxSum) {
-            // maxSum = sm;
-            // }
-            // }
-            // }
-            // System.out.print("Max sum is:" + maxSum);
-
-            // // 2.Prefix array apprioach: O(n^2)
-            // int numb[] = { -1, -2, -3 };
-            // int currSum = 0;
-            // int prefix[] = new int[numb.length];
-            // prefix[0] = numb[0];
-            // for (int i = 1; i < numb.length; i++) {
-            // prefix[i] = numb[i] + prefix[i - 1];
-            // }
-
-            // for (int i = 0; i < numb.length; i++) {
-            // for (int j = i; j < numb.length; j++) {
-            // if (i == 0) {
-            // currSum = prefix[j];
-            // } else {
-            // currSum = prefix[j] - prefix[i - 1];
-            // }
-            // maxSum = Math.max(currSum, maxSum);
-            // }
-
-            // }
-            // System.out.println();
-            // System.out.print(maxSum);
-
-            // // 3.Kdanes algo:
-            // int ms = Integer.MIN_VALUE;
-            // int cs = 0;
-
-            // for (int i = 0; i < numb.length; i++) {
-            // cs += numb[i];
-            // ms = Math.max(ms, cs);
-            // if (cs < 0) {
-            // cs = 0;
-            // }
-            // }
-            // System.out.println("ms kdanes algo: " + ms);
-
-            // int[] prices = { 7, 1, 4, 3, 6, 2 };
-            // int n = prices.length;
-            // int profit = 0;
-            // int maxProfit = 0;
-            // int minPrice = prices[0];
-            // for (int i = 1; i < n; i++) {
-            // profit = prices[i] - minPrice;
-            // maxProfit = Math.max(maxProfit, profit);
-            // minPrice = Math.min(minPrice, prices[i]);
-            // }
-            // System.out.println(maxProfit);
-
-            // // Count Sort:
-            // int sort[] = { 5, 4, 3 };
-            // int max = sort[0];
-            // for (int i = 1; i < sort.length; i++) {
-            // max = Math.max(max, arr[i]);
-            // }
-
-            // int count[] = new int[max + 1];
-            // for (int i = 0; i < sort.length; i++) {
-            // count[sort[i]]++;
-            // }
-
-            // int i = 0;
-            // for (int j = 0; j < count.length; j++) {
-            // while (count[j] != 0) {
-            // sort[i] = j;
-            // i++;
-            // count[j]--;
-            // }
-            // }
-
-            // for (int x : sort) {
-            // System.out.print(x + " ");
-            // }
-
-            // 2D arrays:
-            // creation: 2d array of 5 rows and columns:
-            int[][] array2d = new int[5][5]; // all zero elements
-            int[][] arr2d = { { 1, 2 }, { 3, 4 }, { 5, 6 } };
-            // accessing elements:
-            int xy = arr2d[0][0];
-            System.out.println(xy);
-            // updating elements:
-            arr2d[0][0] = 100;
-            // rows and columns in 2darrays:
-            int row = arr2d.length; // gives rows
-            int col = arr2d[0].length; // gives colms
-            System.out.println(row + " " + col);
-            // printing 2d array:
-            for (int i = 0; i < arr2d.length; i++) {
-                  for (int j = 0; j < arr2d[i].length; j++) {
-                        System.out.print(arr2d[i][j] + " ");
-                  }
+            // 3.printing array:
+            for (int i = 0; i < arr.length; i++) {
+                  System.out.print(arr[i] + " ");
+            }
+            System.out.println();
+            for (int nums : arr1) {
+                  System.out.print(nums + " ");
             }
             System.out.println();
 
-            // jagged array: rows can have diff length;
-            int jagged[][] = {
-                        { 1, 2, 3 },
-                        { 4 }
-            };
-            for (int i = 0; i < jagged.length; i++) {
-                  for (int j = 0; j < jagged[i].length; j++) {
-                        System.out.print(jagged[i][j] + " ");
-                  }
+            // 4.update in arrays:
+            arr[2] = 100;
+            for (int nums : arr) {
+                  System.out.print(nums + " ");
             }
             System.out.println();
 
+            // 5.input from user:
+            // Scanner sc = new Scanner(System.in);
+            // int length = 5;
+            // int[] nums = new int[length];
+            // for(int i=0;i<length;i++){
+            // System.out.print("Enter Element: ");
+            // nums[i] = sc.nextInt();
+            // }
+            // for(int x: nums){
+            // System.out.print(x+" ");
+            // }
+            // System.out.println();
+
+            // 6.arrays as function arguments:
+            // normal pass by refernce fnxs:
+            int a = 100;
+            passByValue(a); // reference of the val
+            System.out.println(a); // op:100
+
+            // pass by value
+            int arr2[] = { 1, 2, 3 };
+            passByRef(arr2); // value is copied
+            for (int nums : arr2) { // op: 1 200 3
+                  System.out.print(nums + " ");
+            }
+            System.out.println();
+
+            // 7.Searching in arrays:
+            // 7.1 Linear Searching: O(n)
+            int[] arr3 = { 100, 200, 300, 400 };
+            int target = 300;
+            for (int i = 0; i < arr3.length; i++) {
+                  if (arr3[i] == target) {
+                        System.out.println(i);
+                        break;
+                  }
+            }
+
+            // 7.2 Binary Search: O(logn)
+            int low = 0;
+            int high = arr3.length - 1;
+            while (low <= high) {
+                  int mid = low + (high - low) / 2;
+                  if (target == arr3[mid]) {
+                        System.out.println(mid);
+                        break;
+                  } else if (target > arr3[mid]) {
+                        low = mid + 1;
+                  } else {
+                        high = mid - 1;
+                  }
+            }
+
+            // 8.Sorting in arrays:
+            // 8.1: Bubble Sort -> O(n^2)
+            int arr4[] = { 1, 5, 3, 2, 4 };
+            for (int i = 0; i < arr4.length - 1; i++) {
+                  for (int j = 0; j < arr4.length - i - 1; j++) {
+                        if (arr4[j] > arr4[j + 1]) {
+                              int temp = arr4[j];
+                              arr4[j] = arr4[j + 1];
+                              arr4[j + 1] = temp;
+                        }
+                  }
+            }
+            for (int nums : arr4) {
+                  System.out.print(nums + " ");
+            }
+            System.out.println();
+
+            // 8.2: Selection Sort -> O(n^2)
+            int arr5[] = { 500, 100, 200 };
+            for (int i = 0; i < arr5.length - 1; i++) {
+                  int minIdx = i;
+                  for (int j = i + 1; j < arr5.length; j++) {
+                        if (arr5[minIdx] > arr5[j]) {
+                              minIdx = j;
+                        }
+                  }
+                  int temp = arr5[i];
+                  arr5[i] = arr5[minIdx];
+                  arr5[minIdx] = temp;
+            }
+            for (int nums : arr5) {
+                  System.out.print(nums + " ");
+            }
+            System.out.println();
+
+            // 8.3: Insertion Sort-> O(n^2)
+            int arr6[] = { -1, 8, 0, -2, 3 };
+            for (int i = 1; i < arr6.length; i++) {
+                  int minEle = arr6[i];
+                  int j = i - 1;
+                  while (j >= 0 && arr6[j] > minEle) {
+                        arr6[j + 1] = arr6[j];
+                        j--;
+                  }
+                  arr6[j + 1] = minEle;
+            }
+            for (int nums : arr6) {
+                  System.out.print(nums + " ");
+            }
+            System.out.println();
+
+            // 8.4:Count Sort->O(n+k) + O(n+k)
+            // n->no of elements in array
+            // k->max no in array
+
+            int maximum = 0;
+            for (int nums : arr4) {
+                  maximum = Math.max(maximum, nums);
+            }
+            int[] count = new int[maximum + 1];
+            for (int i = 0; i < arr4.length; i++) {
+                  count[arr4[i]]++;
+            }
+
+            int x = 0;
+            for (int i = 0; i < count.length; i++) {
+                  while (count[i] > 0) {
+                        arr4[x] = i;
+                        x++;
+                        count[i]--;
+                  }
+            }
+            for (int nums : arr4) {
+                  System.out.print(nums + " ");
+            }
+            System.out.println();
+
+            // Questions by AC:
+            // 1.largest in array: O(n)
+            int[] uni = { 1, 2, 5, 3, 4 };
+            int max = arr[0];
+            for (int i = 0; i < uni.length; i++) {
+                  if (uni[i] > max) {
+                        max = uni[i];
+                  }
+            }
+            System.out.println(max);
+
+            // 2.reverse array: O(n)
+            int start = 0;
+            int end = uni.length - 1;
+            while (start < end) {
+                  int temp = uni[start];
+                  uni[start] = uni[end];
+                  uni[end] = temp;
+                  start++;
+                  end--;
+            }
+            for (int nums : uni) {
+                  System.out.print(nums + " ");
+            }
+            System.out.println();
+
+            // 3.print pairs: O(n^2)
+            for (int i = 0; i < uni.length; i++) {
+                  for (int j = i + 1; j < uni.length; j++) {
+                        System.out.print("(" + uni[i] + "," + uni[j] + ")");
+                  }
+                  System.out.println();
+            }
+
+            // 4.print subarrays: O(n^3)
+            for (int i = 0; i < uni.length; i++) {
+                  for (int j = i; j < uni.length; j++) {
+                        for (int k = i; k <= j; k++) {
+                              System.out.print(uni[k]);
+                        }
+                        System.out.println();
+                  }
+                  System.out.println();
+            }
+
+            // 5.Max Subarray sum
+            // 5.1: Brute Force: O(n^3)
+            int maxSum = Integer.MIN_VALUE;
+            int currSum = 0;
+            for (int i = 0; i < uni.length; i++) {
+                  for (int j = i; j < uni.length; j++) {
+                        currSum = 0;
+                        for (int k = i; k <= j; k++) {
+                              currSum += uni[k];
+                              maxSum = Math.max(currSum, maxSum);
+                        }
+                  }
+            }
+            System.out.println(maxSum);
+
+            // 5.2: Prefix sum: O(n^2) + O(n)
+            int[] prefix = new int[uni.length];
+            prefix[0] = uni[0];
+            for (int i = 1; i < uni.length; i++) {
+                  prefix[i] = uni[i] + prefix[i - 1];
+            }
+            for (int i = 0; i < uni.length; i++) {
+                  for (int j = i; j < uni.length; j++) {
+                        if (i == 0) {
+                              currSum = prefix[j];
+                        } else {
+                              currSum = prefix[j] - prefix[i - 1];
+                        }
+                        maxSum = Math.max(currSum, maxSum);
+                  }
+            }
+            System.out.println(maxSum);
+
+            // 5.3 Kdane's Algorithm: O(n)
+            for (int i = 0; i < uni.length; i++) {
+                  currSum += uni[i];
+                  maxSum = Math.max(currSum, maxSum);
+                  if (currSum < 0) {
+                        currSum = 0;
+                  }
+            }
+            System.out.println(maxSum);
+
+            // 6.Best time to buy and Sell Stocks:
+            // 6.1: Brute force approach -> O(n^2)
+            int[] prices = { 7, 1, 4, 5, 6, 2 };
+            int maxProfit = Integer.MIN_VALUE;
+            int profit = 0;
+            for (int i = 0; i < prices.length; i++) {
+                  for (int j = i + 1; j < prices.length; j++) {
+                        profit = prices[j] - prices[i];
+                        maxProfit = Math.max(profit, maxProfit);
+                  }
+            }
+            System.out.println(maxProfit);
+
+            // 6.2: Optimal approach-> O(n)
+            int minPrice = prices[0];
+            for (int i = 1; i < prices.length; i++) {
+                  profit = prices[i] - minPrice;
+                  maxProfit = Math.max(profit, maxProfit);
+                  minPrice = Math.min(minPrice, prices[i]);
+            }
+            System.out.println(maxProfit);
       }
 }
